f00685f5865b5f56db1a94301001c77528e5b3fb
hae-gun <newchk610@gmail.com>
2022-01-03 23:55:04 +0900

    algo

---
+++ leet_997.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        for line in trust:
+            print(line)
+        
