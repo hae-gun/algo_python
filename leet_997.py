19568ca0d7c9a7c5f02837d4a67d355e1de2abfc
hae-gun <newchk610@gmail.com>
2022-01-09 23:10:02 +0900

    python class practice

--- leet_997.py
+++ leet_997.py
@@ -1,10 +1,24 @@
-class Solution(object):
-    def findJudge(self, n, trust):
-        """
-        :type n: int
-        :type trust: List[List[int]]
-        :rtype: int
-        """
-        for line in trust:
-            print(line)
-        
+class Vertex:
+    def __init__(self, label):
+        self.label = label
+
+    def show(self):
+        return self.label
+
+
+class Graph:
+    def __init__(self):
+        self.in_degree = list()
+        self.out_degree = list()
+
+    def get_in_degree(self):
+        return self.in_degree
+
+    def get_out_degree(self):
+        return self.out_degree
+
+var1 = Vertex(10)
+print(var1.show())
+
+# class Solution:
+#    def findJudge(self, n: int, trust: List[List[int]]) -> int:
